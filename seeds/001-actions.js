30102e56fa06fd19b98a604c78787bde31f3668b
coriagogo <pkcparis@gmail.com>
2019-06-07 13:09:25 -0400

    actions seeded

--- seeds/001-actions.js
+++ seeds/001-actions.js
@@ -1,13 +1,29 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('actions').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('actions').insert([
+        {
+          project_id: 1, 
+          description: 'Describe action 1',
+          notes: 'Notes for action 1',
+          completed: false
+        },
+        {
+          project_id: 1, 
+          description: 'Describe action 2',
+          notes: 'Notes for action 2',
+          completed: false
+        },
+        {
+          project_id: 1, 
+          description: 'Describe action 2',
+          notes: 'Notes for action 2',
+          completed: false
+        },
+        
       ]);
     });
 };
